4b74d86dcc1d6d267bb20c8a99cdb7a512d8f2ee
Athar-ali-siddiqui <65170037+Athar-ali-siddiqui@users.noreply.github.com>
2020-08-03 00:07:48 +0500

    Animated Blogger web

--- practice/Blogger-web/script.js
+++ practice/Blogger-web/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      500: {
+        items: 2,
+        nav: true,
+        loop: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1500: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+  AOS.init();
 });
